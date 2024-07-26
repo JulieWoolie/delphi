7a45d5e63927ca53ef8f94d970bd88d3eec7f68f
andero-magi <andero.magi.420@gmail.com>
2024-07-26 11:24:45 +0300

    Testing support

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,25 @@ subprojects {
 
   dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+
+    // Test dependencies
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.3")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("com.google.guava:guava:33.2.1-jre")
+    testImplementation("org.slf4j:slf4j-api:2.0.13")
+    testImplementation("org.slf4j:slf4j-simple:2.0.13")
+    testImplementation("it.unimi.dsi:fastutil:8.5.13")
+    testImplementation("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+  }
+
+  tasks {
+    test {
+      useJUnitPlatform()
+      systemProperty("org.slf4j.simpleLogger.defaultLogLevel", "trace")
+    }
   }
 
   java {
