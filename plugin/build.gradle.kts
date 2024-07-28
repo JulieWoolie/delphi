ba6135563fce998a0fbcd59eb24f27102159889a
andero-magi <andero.magi.420@gmail.com>
2024-07-28 19:30:05 +0300

    Use grenadier in plugin

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,11 +1,17 @@
 plugins {
   java
+  id("io.freefair.lombok") version "8.6"
 }
 
 repositories {
   mavenCentral()
+  maven("https://libraries.minecraft.net/")
 }
 
 dependencies {
+  implementation(project(":dom"))
+  implementation(project(":api"))
 
+  compileOnly("net.forthecrown:grenadier:2.5.2")
+  testImplementation("net.forthecrown:grenadier:2.5.2")
 }
