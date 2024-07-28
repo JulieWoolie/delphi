d550fd8edbdcbe3eff9bfc19e7ab69a5157024aa
andero-magi <andero.magi.420@gmail.com>
2024-07-28 19:19:59 +0300

    Add documentation and use result types instead of optional

--- api/src/main/java/net/arcadiusmc/delphi/resource/DelphiResources.java
+++ api/src/main/java/net/arcadiusmc/delphi/resource/DelphiResources.java
@@ -2,7 +2,7 @@ package net.arcadiusmc.delphi.resource;
 
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Optional;
+import net.arcadiusmc.delphi.util.Result;
 import org.jetbrains.annotations.NotNull;
 
 public interface DelphiResources {
@@ -29,7 +29,7 @@ public interface DelphiResources {
    *         {@code false}, if the specified name is already in use.
    *
    * @throws NullPointerException If either {@code moduleName} or {@code module} is {@code null}.
-   * @throws IllegalArgumentException If the module name doesn't pass the {@link PagePath#validateQuery(String)} check.
+   * @throws IllegalArgumentException If the module name doesn't pass the {@link ResourcePath#validateQuery(String)} check.
    */
   boolean registerModule(@NotNull String moduleName, @NotNull ResourceModule module);
 
@@ -42,10 +42,38 @@ public interface DelphiResources {
    * <p>
    * This method assumes the input has no file extension.
    *
+   * <table>
+   *   <caption>Result errors</caption>
+   *   <tr>
+   *     <th>Message format</th>
+   *     <th>Description</th>
+   *   </tr>
+   *   <tr>
+   *     <td>{@code "Null/empty module name"}</td>
+   *     <td>The module name was empty or {@code null}</td>
+   *   </tr>
+   *   <tr>
+   *     <td>{@code "Module directory doesn't exist"}</td>
+   *     <td>Couldn't find any registered modules matching the name, and the module directory doesn't exist</td>
+   *   </tr>
+   *   <tr>
+   *     <td>{@code "Zip access denied: %reason%"}</td>
+   *     <td>Attempted to open a {@code .zip} module file, but access was denied</td>
+   *   </tr>
+   *   <tr>
+   *     <td>{@code "IO Error: %message%"}</td>
+   *     <td>An IO error occurred while trying to read a {@code .zip} module file</td>
+   *   </tr>
+   *   <tr>
+   *     <td>{@code "Unknown Module"}</td>
+   *     <td>No registered module, directory or {@code .zip} module was found that matched the specified {@code moduleName}</td>
+   *   </tr>
+   * </table>
+   *
    * @param moduleName Name of the module
-   * @return Found module, or an {@code empty} optional if no module was found.
+   * @return Found module, or an erroneous result if no module was found.
    */
-  Optional<ResourceModule> findModule(String moduleName);
+  Result<ResourceModule, String> findModule(String moduleName);
 
   /**
    * Gets an array list of all module names. This list inclues all registered modules
