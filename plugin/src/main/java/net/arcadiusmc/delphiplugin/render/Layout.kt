dd510c0f0eb9b8b9a1aa8afdaa09e008360bf3a8
andero-magi <andero.magi.420@gmail.com>
2024-08-15 12:04:27 +0300

    Implement the most basic Flexbox algorithm + fix top margins being ignored

--- plugin/src/main/java/net/arcadiusmc/delphiplugin/render/Layout.kt
+++ plugin/src/main/java/net/arcadiusmc/delphiplugin/render/Layout.kt
@@ -1,8 +1,9 @@
 package net.arcadiusmc.delphiplugin.render
 
 import net.arcadiusmc.delphiplugin.math.Rectangle
-import net.arcadiusmc.dom.style.DisplayType
+import net.arcadiusmc.dom.style.*
 import org.joml.Vector2f
+import java.util.*
 import kotlin.math.max
 import kotlin.math.min
 
@@ -47,8 +48,8 @@ fun postAlign(el: ElementRenderObject) {
   for (child in el.childObjects) {
     child.getBounds(rectangle)
 
-    childMax.x = rectangle.getPosition().x + rectangle.getSize().x
-    childMax.y = rectangle.getPosition().y
+    childMax.x = rectangle.position.x + rectangle.size.x
+    childMax.y = rectangle.position.y
 
     bottomRight.x = max(childMax.x, bottomRight.x)
     bottomRight.y = min(childMax.y, bottomRight.y)
@@ -104,10 +105,115 @@ fun layoutFlex(el: ElementRenderObject) {
   val maxSize: Vector2f = Vector2f()
   maxSize(el, maxSize)
 
+  val style = el.style
 
+  // Content alignment along the Flex box's axis (column or row)
+  val justify = style.justfiy
+
+  // Content alignment along the Flex box's cross axis
+  val alignItems = style.alignItems
+
+  // Direction the content is aligned in,
+  val direction: FlexDirection = style.flexDirection
+  val wrap: FlexWrap = style.flexWrap
+
+  // Notes:
+  // For now, ignore margins, implement them later, just get one or two of the alignment modes
+  // functioning.
+  //
+  //
+  //
+
+  // TODO: Add support for column direction
+  if (direction != FlexDirection.ROW && direction != FlexDirection.ROW_REVERSE) {
+    layoutFlow(el)
+    return
+  }
+
+  // TODO
+  if (justify != JustifyContent.CENTER
+      && justify != JustifyContent.FLEX_END
+      && justify != JustifyContent.FLEX_START
+  ) {
+    layoutFlow(el)
+    return
+  }
+
+  if (alignItems != AlignItems.FLEX_START) {
+    layoutFlow(el)
+    return
+  }
+
+  val lines: MutableList<RenderLine>
+
+  if (wrap == FlexWrap.NOWRAP) {
+    lines = ArrayList()
+
+    val line = RenderLine()
+    val childSize = Vector2f()
+    line.objects.addAll(el.childObjects)
+
+    lines.add(line)
+
+    for (childObject in el.childObjects) {
+      val style = childObject.style
+      line.bottomMargin = max(style.margin.bottom, line.bottomMargin)
+
+      childObject.getElementSize(childSize)
+      line.size.x += style.margin.left + style.margin.right + childSize.x
+      line.size.y = max(line.size.y, childSize.y + style.margin.top)
+    }
+  } else {
+    lines = splitIntoLines(el)
+
+    if (wrap == FlexWrap.WRAP_REVERSE) {
+      Collections.reverse(lines)
+    }
+  }
+
+  if (direction == FlexDirection.ROW_REVERSE) {
+    for (line in lines) {
+      Collections.reverse(line.objects)
+    }
+  }
+
+  val start: Vector2f = Vector2f()
+  val childPos: Vector2f = Vector2f()
+  val childSize: Vector2f = Vector2f()
+  val advance: Vector2f = Vector2f()
+
+  el.getContentStart(start)
+
+  for (line in lines) {
+    val xDif = maxSize.x - line.size.x
+
+    if (justify == JustifyContent.CENTER) {
+      advance.x = xDif * 0.5f;
+    } else if (justify == JustifyContent.FLEX_END) {
+      advance.x = xDif;
+    } else {
+      advance.x = 0.0f
+    }
+
+    for (obj in line.objects) {
+      obj.getElementSize(childSize)
+
+      childPos.set(start)
+      childPos.x += advance.x
+      childPos.x += obj.style.margin.left
+
+      childPos.y -= advance.y
+
+      obj.moveTo(childPos)
+
+      advance.x += obj.style.margin.left + obj.style.margin.right + childSize.x
+    }
+
+    advance.y += line.size.y + line.bottomMargin
+  }
 }
 
-fun splitIntoLines(el: ElementRenderObject): List<RenderLine> {
+fun splitIntoLines(el: ElementRenderObject): MutableList<RenderLine> {
   val lines: MutableList<RenderLine> = ArrayList()
   var line: RenderLine? = null
 
@@ -140,6 +246,7 @@ fun splitIntoLines(el: ElementRenderObject): List<RenderLine> {
       if (display == DisplayType.BLOCK || display == DisplayType.FLEX) {
         line.objects.add(child)
         line.size.set(childSize)
+        line.size.y += style.margin.top
         line.bottomMargin = style.margin.bottom
         lines.add(line)
 
@@ -149,7 +256,7 @@ fun splitIntoLines(el: ElementRenderObject): List<RenderLine> {
     }
 
     line.size.x += childSize.x + style.margin.left + style.margin.right
-    line.size.y = max(line.size.y, childSize.y)
+    line.size.y = max(line.size.y, childSize.y + style.margin.top)
     line.bottomMargin = max(line.bottomMargin, style.margin.bottom)
 
     line.objects.add(child)
