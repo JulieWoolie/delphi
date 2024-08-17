9eefb7401d59793294554248f730ca87bd0ee51f
andero-magi <andero.magi.420@gmail.com>
2024-08-17 21:26:27 +0300

    Add `ignore-player-pitch` option

--- api/src/main/java/net/arcadiusmc/dom/Options.java
+++ api/src/main/java/net/arcadiusmc/dom/Options.java
@@ -20,12 +20,16 @@ public interface Options {
   /**
    * Screen width option. By default, the value of this option is {@link Screen#DEFAULT_WIDTH}
    * <p>
+   * Modifying this option when a page is already open, will cause the screen's size to change
+   * <p>
    * Example: {@code <option name="screen-width" value="3"/>}
    */
   String SCREEN_WIDTH = "screen-width";
 
   /**
    * Screen height option. By default, the value of this option is {@link Screen#DEFAULT_HEIGHT}
+   * <p>
+   * Modifying this option when a page is already open, will cause the screen's size to change
    * <p>
    * Example: {@code <option name="screen-height" value="2"/>}
    */
@@ -40,4 +44,17 @@ public interface Options {
    * Example: {@code <option name="advanced-item-tooltips" value="true"/>}
    */
   String ADVANCED_ITEM_TOOLTIPS = "advanced-item-tooltips";
+
+  /**
+   * Ignore the pitch of the player when opening the screen. Setting this to {@code false} means
+   * the screen will spawn in front of the player regardless of what direction they're looking in.
+   * <p>
+   * If the value is {@code true}, then the screen will be spawned in front of the player at their
+   * eye level without any X-axis rotation.
+   * <p>
+   * By default, the value of the option is {@code true}
+   * <p>
+   * Example: {@code <option name="ignore-player-pitch" value="false" />}
+   */
+  String IGNORE_PLAYER_PITCH = "ignore-player-pitch";
 }
