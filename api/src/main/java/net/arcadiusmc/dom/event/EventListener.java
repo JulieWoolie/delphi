cb2286f4c161dd2379c1d628d769536848c59d6e
andero-magi <andero.magi.420@gmail.com>
2024-08-28 18:04:35 +0300

    Add documentation to EventListener

--- api/src/main/java/net/arcadiusmc/dom/event/EventListener.java
+++ api/src/main/java/net/arcadiusmc/dom/event/EventListener.java
@@ -1,11 +1,32 @@
 package net.arcadiusmc.dom.event;
 
+import org.jetbrains.annotations.ApiStatus.OverrideOnly;
+
+/**
+ * Event listener functional interface
+ */
+@FunctionalInterface
+@OverrideOnly
 public interface EventListener {
 
+  /**
+   * Handle the event
+   * @param event Event
+   */
   void onEvent(Event event);
 
+  /**
+   * Typed event listener
+   * @param <T> Event type
+   */
+  @FunctionalInterface
+  @OverrideOnly
   interface Typed<T extends Event> extends EventListener {
 
+    /**
+     * Handle the event
+     * @param event Event
+     */
     void handleEvent(T event);
 
     @Override
