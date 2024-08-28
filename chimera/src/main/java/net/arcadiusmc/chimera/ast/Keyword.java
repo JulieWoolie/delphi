233b9c6471160e406b78bb1f735ae3d4a4016e16
andero-magi <andero.magi.420@gmail.com>
2024-08-28 20:21:27 +0300

    Move Keyword class and add boolean keywords

---
+++ chimera/src/main/java/net/arcadiusmc/chimera/ast/Keyword.java
@@ -0,0 +1,42 @@
+package net.arcadiusmc.chimera.ast;
+
+public enum Keyword {
+  INHERIT,
+  INITIAL,
+  AUTO,
+  UNSET,
+
+  // Align items
+  FLEX_START,
+  FLEX_END,
+  CENTER,
+  STRETCH,
+  BASELINE,
+
+  // Display options
+  NONE,
+  INLINE,
+  BLOCK,
+  INLINE_BLOCK,
+  FLEX,
+
+  // Flex types
+  ROW,
+  ROW_REVERSE,
+  COLUMN,
+  COLUMN_REVERSE,
+
+  // Flex wrap
+  NOWRAP,
+  WRAP,
+  WRAP_REVERSE,
+
+  // Justify content
+  SPACE_BETWEEN,
+  SPACE_AROUND,
+  SPACE_EVENLY,
+
+  // Boolean values
+  TRUE,
+  FALSE
+}
