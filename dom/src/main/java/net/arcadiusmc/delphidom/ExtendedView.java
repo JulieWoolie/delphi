3b988b3fe7ee2c090d5737cd2a08cd7d985e132e
andero-magi <andero.magi.420@gmail.com>
2024-09-11 17:22:34 +0300

    Remove style update callbacks from ExtendedView

--- dom/src/main/java/net/arcadiusmc/delphidom/ExtendedView.java
+++ dom/src/main/java/net/arcadiusmc/delphidom/ExtendedView.java
@@ -1,15 +1,10 @@
 package net.arcadiusmc.delphidom;
 
-import java.util.Map;
 import net.arcadiusmc.delphi.DocumentView;
 
 public interface ExtendedView extends DocumentView {
 
-  void styleUpdated(DelphiNode node, int changes);
-
   void contentChanged(DelphiNode text);
-
-  Map<String, Object> getStyleVariables();
 
   void removeRenderElement(DelphiElement element);
 
