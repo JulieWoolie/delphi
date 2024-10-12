39889eb19ade1d9165506e22072b3e37c441eee6
andero-magi <73311304+JulieWoolie@users.noreply.github.com>
2024-10-12 13:32:38 +0300

    Add more color tests

--- api/src/test/java/net/arcadiusmc/dom/style/ColorTest.java
+++ api/src/test/java/net/arcadiusmc/dom/style/ColorTest.java
@@ -1,6 +1,7 @@
 package net.arcadiusmc.dom.style;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,45 @@ class ColorTest {
     assertEquals("red", NamedColor.RED.toString());
     assertEquals("red", Color.rgb(255, 0, 0).toString());
     assertEquals("#ffccff", Color.rgb(0x00FFCCFF).toString());
+  }
+
+  @Test
+  void rgb() {
+    Color c = Color.rgb(255, 10, 10);
+    assertEquals(255, c.getRed());
+    assertEquals(10, c.getGreen());
+    assertEquals(10, c.getBlue());
+
+    c = Color.rgb(0xFFc0c0);
+    assertEquals(0xFF, c.getRed());
+    assertEquals(0xc0, c.getGreen());
+    assertEquals(0xc0, c.getBlue());
+  }
+
+  @Test
+  void argb() {
+    Color c = Color.argb(25, 255, 10, 10);
+    assertEquals(25, c.getAlpha());
+    assertEquals(255, c.getRed());
+    assertEquals(10, c.getGreen());
+    assertEquals(10, c.getBlue());
+
+    c = Color.argb(0xabFFc0c0);
+    assertEquals(0xab, c.getAlpha());
+    assertEquals(0xFF, c.getRed());
+    assertEquals(0xc0, c.getGreen());
+    assertEquals(0xc0, c.getBlue());
+  }
+
+  @Test
+  void brighten() {
+    Color pre = NamedColor.DARK_GREEN;
+    Color after = pre.brighten();
+
+    assertNotEquals(pre, after);
+  }
+
+  @Test
+  void darken() {
   }
 }
