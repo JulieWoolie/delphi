0ca3d993cb3d0571e1e3324bad63c4f2a1ccd3d8
andero-magi <73311304+JulieWoolie@users.noreply.github.com>
2024-10-12 18:49:13 +0300

    Bump DOM and API versions

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
   `java-library`
+  `maven-publish`
 }
+
+version = "1.0.1-SNAPSHOT"
 
 repositories {
   mavenCentral()
@@ -8,4 +11,31 @@ repositories {
 
 dependencies {
 
+}
+
+java {
+  withSourcesJar()
+  withJavadocJar()
+}
+
+tasks {
+  javadoc {
+    isFailOnError = false
+  }
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("mavenJava") {
+      from(components["java"])
+      artifactId = "delphi"
+    }
+  }
+
+  repositories {
+    maven {
+      // Define the local directory where you want to publish the artifact
+      url = uri(layout.buildDirectory.dir("maven-repo"))
+    }
+  }
 }
