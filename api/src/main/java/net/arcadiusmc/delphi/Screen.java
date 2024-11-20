d45a6ae2d03d154e735245403fafb1fae21bba7e
andero-magi <andero.magi.420+schoolgit@gmail.com>
2024-11-20 13:54:30 +0200

    Improve Screen javadoc

--- api/src/main/java/net/arcadiusmc/delphi/Screen.java
+++ api/src/main/java/net/arcadiusmc/delphi/Screen.java
@@ -5,6 +5,25 @@ import org.joml.Vector3f;
 
 /**
  * The plane a {@link DocumentView} exists on.
+ * <p>
+ * Screen coordinates exist in one of 3 spaces
+ * <dl>
+ *   <dt>World space coordinates</dt>
+ *   <dd>They denote the actual size and dimensions of a screen in a minecraft world.</dd>
+ *
+ *   <dt>Intermediate coordinates</dt>
+ *   <dd>
+ *     Set with the {@code screen-width} and {@code screen-height} document options. They may not
+ *     always be actual sizes of the screen.
+ *   </dd>
+ *
+ *   <dt>Screen coordinates</dt>
+ *   <dd>{@code 0} to {@code 1} coordinates relative to the width and height of the screen.</dd>
+ * </dl>
+ *
+ * Screens store an internal {@link org.bukkit.util.Transformation} that is applied to the screen.
+ * This may mean that (if scaling is involved) world and intermediate coordinates may differ in
+ * sizes.
  */
 public interface Screen {
 
@@ -36,7 +55,7 @@ public interface Screen {
   float getWorldHeight();
 
   /**
-   * Gets the normal of the screen's plane
+   * Gets the normal (direction) of the screen's plane
    * @return Screen normal
    */
   Vector3f normal();
@@ -78,7 +97,8 @@ public interface Screen {
   Vector3f getUpperRight();
 
   /**
-   * Maps screen coordinates in range [0..{@link #getDimensions()}] to world coordinates.
+   * Maps screen coordinates in range [0..{@link #getDimensions()}] to world coordinates and stores
+   * the result in the {@code out} argument.
    *
    * @param screenPoint Screen point, in space [0..{@link #getDimensions()}]
    * @param out Result destination
@@ -86,7 +106,8 @@ public interface Screen {
   void screenToWorld(Vector2f screenPoint, Vector3f out);
 
   /**
-   * Maps screen coordinates in range [0..{@link #getDimensions()}] to [0..1] space.
+   * Maps screen coordinates in range [0..{@link #getDimensions()}] to [0..1] space and stores the
+   * result in the {@code out} argument.
    *
    * @param in Input in range [0..{@link #getDimensions()}]
    * @param out Result output, in range [0..1]
@@ -94,7 +115,8 @@ public interface Screen {
   void screenToScreenspace(Vector2f in, Vector2f out);
 
   /**
-   * Maps screen coordinates in range [0..1] to [0..{@link #getDimensions()}] space.
+   * Maps screen coordinates in range [0..1] to [0..{@link #getDimensions()}] space and stores the
+   * result in the {@code out} argument.
    *
    * @param in Input in range [0..1]
    * @param out Result output, in range [0..{@link #getDimensions()}]
@@ -102,7 +124,8 @@ public interface Screen {
   void screenspaceToScreen(Vector2f in, Vector2f out);
 
   /**
-   * Maps screen coordinates in range [0..1] to world coordinates.
+   * Maps screen coordinates in range [0..1] to world coordinates and stores the
+   * result in the {@code out} argument.
    *
    * @param screenPoint Screen point, in space [0..1]
    * @param out Result destination
