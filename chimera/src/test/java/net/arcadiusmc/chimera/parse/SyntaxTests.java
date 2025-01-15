5a88ef140d0804aa9c22cb1e150aaed3486d0ee1
andero-magi <andero.magi.420+schoolgit@gmail.com>
2025-01-15 16:26:22 +0200

    Add simple dynamic test loader to chimera

---
+++ chimera/src/test/java/net/arcadiusmc/chimera/parse/SyntaxTests.java
@@ -0,0 +1,228 @@
+package net.arcadiusmc.chimera.parse;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.DirectoryStream;
+import java.nio.file.DirectoryStream.Filter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+import net.arcadiusmc.chimera.parse.ast.SheetStatement;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.function.Executable;
+import org.slf4j.event.Level;
+
+public class SyntaxTests {
+
+  static final String SUFFIX = ".test.json";
+  static final Filter<Path> TEST_JSON_FILTER = entry -> {
+    if (Files.isDirectory(entry)) {
+      return true;
+    }
+
+    return entry.toString().endsWith(SUFFIX);
+  };
+
+  @TestFactory
+  Iterable<DynamicTest> generateTests() throws Exception {
+    String resourceUrl = getClass().getClassLoader().getResource("syntax-tests/").getFile().substring(1);
+    Path resourcesDirectory = Path.of(resourceUrl);
+
+    List<DynamicTest> tests = new ObjectArrayList<>();
+
+    loadFromDir(resourcesDirectory, resourcesDirectory, l -> {
+      DynamicTest t = DynamicTest.dynamicTest(l.displayName, l.fileName.toUri(), l);
+      tests.add(t);
+    });
+
+    return tests;
+  }
+
+  private void loadFromDir(Path root, Path dir, Consumer<LoadedSyntaxTest> consumer)
+      throws IOException
+  {
+    String relPath = root.relativize(dir).toString().replace("\\", "/");
+
+    if (!relPath.isEmpty()) {
+      relPath += "/";
+    }
+
+    try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, TEST_JSON_FILTER)) {
+      for (Path path : stream) {
+        if (Files.isDirectory(path)) {
+          loadFromDir(root, path, consumer);
+          continue;
+        }
+
+        String jsonContent = Files.readString(path, StandardCharsets.UTF_8);
+        JsonObject obj = JsonParser.parseString(jsonContent).getAsJsonObject();
+
+        LoadedSyntaxTest test = new LoadedSyntaxTest();
+        String fname = path.getFileName().toString();
+
+        test.fileName = path;
+        test.expectedResult = obj.get("should-succeed").getAsBoolean();
+        test.displayName = relPath + fname.replace(SUFFIX, "");
+        test.errors = loadErrors(obj);
+
+        if (obj.has("scss")) {
+          test.scssSource = obj.get("scss").getAsString();
+        } else {
+          String scssName = fname.replace(SUFFIX, ".scss");
+
+          Path p = dir.resolve(scssName);
+          if (!Files.exists(p)) {
+            throw new FileNotFoundException(p.toString());
+          }
+
+          test.scssSource = Files.readString(p, StandardCharsets.UTF_8);
+        }
+
+        consumer.accept(test);
+      }
+    }
+  }
+
+  private ExpectedError[] loadErrors(JsonObject object) {
+    if (!object.has("expected-outputs")) {
+      return new ExpectedError[0];
+    }
+
+    JsonArray array = object.getAsJsonArray("expected-outputs");
+    ExpectedError[] errors = new ExpectedError[array.size()];
+
+    for (int i = 0; i < array.size(); i++) {
+      JsonObject json = array.get(i).getAsJsonObject();
+      ExpectedError err = new ExpectedError();
+
+      if (json.has("level")) {
+        String levelName = json.get("level").getAsString();
+        err.level = switch (levelName) {
+          case "error" -> Level.ERROR;
+          case "warn" -> Level.WARN;
+          case "info" -> Level.INFO;
+          default -> null;
+        };
+      }
+
+      if (json.has("message")) {
+        err.message = json.get("message").getAsString();
+      }
+
+      if (json.has("line")) {
+        err.line = json.get("line").getAsInt();
+      }
+      if (json.has("column")) {
+        err.column = json.get("column").getAsInt();
+      }
+
+      errors[i] = err;
+    }
+
+    return errors;
+  }
+
+  static class ExpectedError {
+    Level level;
+    String message;
+    Integer line;
+    Integer column;
+
+    void validate(ChimeraError error) {
+      if (!matches(level, error.getLevel())) {
+        validationFail("message log level", error.getLevel(), level, error);
+      }
+
+      if (!matches(message, error.getMessage())) {
+        validationFail("message", error.getMessage(), message, error);
+      }
+
+      Location location = error.getLocation();
+      int line = location == null ? -1 : location.line();
+      int col = location == null ? -1 : location.column();
+
+      if (!matches(this.line, line)) {
+        validationFail("location.line", line, this.line, error);
+      }
+      if (!matches(this.column, col)) {
+        validationFail("location.column", col, this.column, error);
+      }
+    }
+
+    void validationFail(String name, Object found, Object expected, ChimeraError error) {
+      String message = "Unexpected output %s: [[%s]], expected [[%s]]".formatted(name, found, expected);
+      fail(message, new ChimeraException(error));
+    }
+
+    boolean matches(Object predicate, Object object) {
+      if (predicate == null) {
+        return true;
+      }
+      return Objects.equals(predicate, object);
+    }
+  }
+
+  static class LoadedSyntaxTest implements Executable {
+
+    String displayName = "test";
+    boolean expectedResult = true;
+    Path fileName;
+
+    String scssSource = "";
+
+    int outputIdx = 0;
+    ExpectedError[] errors = null;
+
+    public void execute() throws Exception {
+      ChimeraParser parser = new ChimeraParser(scssSource);
+
+      parser.getErrors().setListener(error -> {
+        if (errors == null) {
+          if (error.getLevel() == Level.ERROR) {
+            throw new ChimeraException(error);
+          }
+
+          System.out.println(error.getFormattedError());
+          return;
+        }
+
+        int idx = outputIdx++;
+        if (idx >= errors.length) {
+          fail("Unexpected output", new ChimeraException(error));
+        }
+
+        ExpectedError err = errors[idx];
+        err.validate(error);
+      });
+
+      try {
+        SheetStatement statement = parser.stylesheet();
+
+        System.out.printf("Ran test \"%s\", result=\n", displayName);
+        System.out.println(statement);
+
+        if (expectedResult) {
+          return;
+        }
+
+        fail("Passed when expected to fail");
+      } catch (ChimeraException exception) {
+        if (!expectedResult) {
+          return;
+        }
+
+        fail(exception);
+      }
+    }
+  }
+}
