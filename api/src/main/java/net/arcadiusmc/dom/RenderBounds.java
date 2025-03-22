bd74c7d66cb8ffc9eacb579c6cc8e170f2848ce2
andero-magi <73311304+JulieWoolie@users.noreply.github.com>
2025-03-22 14:23:04 +0200

    Update RenderBounds javadoc

--- api/src/main/java/net/arcadiusmc/dom/RenderBounds.java
+++ api/src/main/java/net/arcadiusmc/dom/RenderBounds.java
@@ -1,9 +1,13 @@
 package net.arcadiusmc.dom;
 
 import org.joml.Vector2f;
+import org.joml.Vector3f;
 
 /**
  * 2D bounding box of coordinates on a {@link net.arcadiusmc.delphi.Screen}.
+ * <p>
+ * Both the minimum and maximum positions can be converted to world coordinates with
+ * {@link net.arcadiusmc.delphi.Screen#screenToWorld(Vector2f, Vector3f)}.
  */
 public interface RenderBounds {
 
