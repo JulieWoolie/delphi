9652243488258fa28e10b682afd7ef4789051f47
JulieWoolie <andero.magi.420@gmail.com>
2025-05-26 13:33:34 +0300

    Integrate scheduler API into JS

---
+++ hephaestus/src/main/java/net/arcadiusmc/hephaestus/stdlib/CancelTask.java
@@ -0,0 +1,22 @@
+package net.arcadiusmc.hephaestus.stdlib;
+
+import lombok.RequiredArgsConstructor;
+import net.arcadiusmc.delphi.DocumentView;
+import org.graalvm.polyglot.Value;
+import org.graalvm.polyglot.proxy.ProxyExecutable;
+
+@RequiredArgsConstructor
+public class CancelTask implements ProxyExecutable {
+
+  private final DocumentView view;
+
+  @Override
+  public Object execute(Value... arguments) {
+    if (arguments.length < 1) {
+      throw new IllegalArgumentException("At least 1 argument required");
+    }
+
+    int taskId = arguments[0].asInt();
+    return view.cancelTask(taskId);
+  }
+}
