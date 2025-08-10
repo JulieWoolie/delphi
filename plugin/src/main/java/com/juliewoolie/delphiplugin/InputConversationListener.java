a6a4e0b94f08976ff84df9816808601fbf884453
JulieWoolie <andero.magi.420@gmail.com>
2025-08-10 14:28:04 +0300

    Replace input element's Bukkit Conversation with new Dialog

--- plugin/src/main/java/com/juliewoolie/delphiplugin/InputConversationListener.java
+++ plugin/src/main/java/com/juliewoolie/delphiplugin/InputConversationListener.java
@@ -1,22 +1,24 @@
 package com.juliewoolie.delphiplugin;
 
-import java.util.Objects;
-import joptsimple.internal.Strings;
 import com.juliewoolie.dom.InputElement;
-import com.juliewoolie.dom.InputElement.InputType;
 import com.juliewoolie.dom.event.EventListener;
 import com.juliewoolie.dom.event.MouseButton;
 import com.juliewoolie.dom.event.MouseEvent;
+import io.papermc.paper.dialog.Dialog;
+import io.papermc.paper.registry.data.dialog.ActionButton;
+import io.papermc.paper.registry.data.dialog.DialogBase;
+import io.papermc.paper.registry.data.dialog.DialogBase.DialogAfterAction;
+import io.papermc.paper.registry.data.dialog.DialogRegistryEntry;
+import io.papermc.paper.registry.data.dialog.action.DialogAction;
+import io.papermc.paper.registry.data.dialog.input.DialogInput;
+import io.papermc.paper.registry.data.dialog.type.DialogType;
+import java.util.List;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import org.bukkit.conversations.Conversation;
-import org.bukkit.conversations.ConversationContext;
-import org.bukkit.conversations.Prompt;
+import net.kyori.adventure.text.event.ClickCallback.Options;
+import net.kyori.adventure.translation.GlobalTranslator;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class InputConversationListener implements EventListener.Typed<MouseEvent> {
 
@@ -40,64 +42,49 @@ public class InputConversationListener implements EventListener.Typed<MouseEvent
     }
 
     Plugin plugin = JavaPlugin.getPlugin(DelphiPlugin.class);
-
     player.playSound(PageInputSystem.CLICK_SOUND);
-    player.sendMessage(Component.translatable("delphi.input.useToClear", NamedTextColor.GRAY));
 
-    Conversation conversation = new Conversation(plugin, player, new DelphiPrompt(el));
-    player.beginConversation(conversation);
+    Dialog dialog = createDialog(player, el);
+    player.showDialog(dialog);
   }
 
-  static class DelphiPrompt implements Prompt {
+  static Component translate(Player audience, String trans) {
+    return GlobalTranslator.render(
+        Component.translatable(trans),
+        audience.locale()
+    );
+  }
 
-    final InputElement inputElement;
-
-    public DelphiPrompt(InputElement inputElement) {
-      this.inputElement = inputElement;
-    }
-
-    @Override
-    public @NotNull String getPromptText(@NotNull ConversationContext context) {
-      return inputElement.getPlaceholder();
-    }
-
-    @Override
-    public boolean blocksForInput(@NotNull ConversationContext context) {
-      return true;
-    }
-
-    @Override
-    public @Nullable Prompt acceptInput(
-        @NotNull ConversationContext context,
-        @Nullable String input
-    ) {
-      if (inputElement.isDisabled()) {
-        return Prompt.END_OF_CONVERSATION;
-      }
-
-      if (Objects.equals(input, CLEAR_CHAR)) {
-        input = null;
-      }
-
-      InputType type = inputElement.getType();
-      Player player = (Player) context.getForWhom();
-
-      if (type == InputType.NUMBER && !Strings.isNullOrEmpty(input)) {
-        try {
-          Double.parseDouble(input);
-        } catch (NumberFormatException exc) {
-          player.sendMessage(
-              Component.translatable("delphi.inputError.invalidNumber")
-                  .color(NamedTextColor.RED)
-                  .arguments(Component.text(input))
-          );
-
-          return Prompt.END_OF_CONVERSATION;
-        }
-      }
-
-      inputElement.setValue(input, player);
-      return Prompt.END_OF_CONVERSATION;
-    }
+  static Dialog createDialog(Player player, InputElement element) {
+    return Dialog.create(factory -> {
+      DialogRegistryEntry.Builder builder = factory.empty();
+      builder.type(
+          DialogType.confirmation(
+              ActionButton.builder(translate(player, "delphi.input.yes"))
+                  .action(DialogAction.customClick(
+                      (response, audience) -> {
+                        element.setValue(response.getText("input_value"), (Player) audience);
+                      },
+                      Options.builder().build()
+                  ))
+                  .build(),
+              ActionButton.builder(translate(player,"delphi.input.no"))
+                  .build()
+          )
+      );
+      builder.base(
+          DialogBase.builder(translate(player,"delphi.input.title"))
+              .inputs(
+                  List.of(
+                      DialogInput.text("input_value", Component.text(element.getPlaceholder()))
+                          .initial(element.getValue())
+                          .build()
+                  )
+              )
+              .afterAction(DialogAfterAction.CLOSE)
+              .canCloseWithEscape(true)
+              .build()
+      );
+    });
   }
 }
