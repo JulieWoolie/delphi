76457f83221c4db17191b5e7e385eec1f76b19b1
JulieWoolie <andero.magi.420@gmail.com>
2025-08-19 17:04:00 +0300

    Add `tagsoup` as test implementation dependency

--- dom/build.gradle.kts
+++ dom/build.gradle.kts
@@ -12,4 +12,5 @@ dependencies {
   implementation(project(":api"))
   implementation(project(":chimera"))
   compileOnly("org.ccil.cowan.tagsoup:tagsoup:1.2.1")
+  testImplementation("org.ccil.cowan.tagsoup:tagsoup:1.2.1")
 }
