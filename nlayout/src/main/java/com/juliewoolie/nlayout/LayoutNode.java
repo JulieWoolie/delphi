2f8a2ca952f2435981d28e1e5f5d44214cd95090
JulieWoolie <andero.magi.420@gmail.com>
2025-08-22 00:30:02 +0300

    Initialise layout node size to `UNSET`

--- nlayout/src/main/java/com/juliewoolie/nlayout/LayoutNode.java
+++ nlayout/src/main/java/com/juliewoolie/nlayout/LayoutNode.java
@@ -5,7 +5,7 @@ import org.joml.Vector2f;
 public abstract class LayoutNode {
 
   public final Vector2f position = new Vector2f();
-  public final Vector2f size = new Vector2f();
+  public final Vector2f size = new Vector2f(LayoutStyle.UNSET);
 
   public int domIndex = 0;
 
