ce3de65194b550604d1e4a25d6607728f0cad697
JulieWoolie <andero.magi.420@gmail.com>
2025-10-12 12:27:36 +0300

    By default, disable Delphi's auto updater

--- plugin/src/main/java/com/juliewoolie/delphiplugin/DelphiConfig.java
+++ plugin/src/main/java/com/juliewoolie/delphiplugin/DelphiConfig.java
@@ -4,9 +4,9 @@ import org.bukkit.configuration.ConfigurationSection;
 
 public class DelphiConfig {
 
-  public boolean autoUpdatePlugin = true;
+  public boolean autoUpdatePlugin = false;
 
   void load(ConfigurationSection sect) {
-    autoUpdatePlugin = sect.getBoolean("enable-auto-updater", true);
+    autoUpdatePlugin = sect.getBoolean("enable-auto-updater", false);
   }
 }
