76250b8a1bbdda2a2b2d7434b3928e90952c41c5
JulieWoolie <andero.magi.420@gmail.com>
2025-11-30 10:30:14 +0200

    Bring the `MICRO_LAYER_DEPTH` back down, the issue was caused by floating point precision loss, which is now fixed

--- render/src/main/java/com/juliewoolie/delphirender/Consts.java
+++ render/src/main/java/com/juliewoolie/delphirender/Consts.java
@@ -114,7 +114,7 @@ public interface Consts {
    * so it doesn't look like elements are floating above other elements too
    * obviously.
    */
-  float MICRO_LAYER_DEPTH = 0.00125f;
+  float MICRO_LAYER_DEPTH = 0.000125f;
 
   /**
    * The distance between each render object on the Z axis.
