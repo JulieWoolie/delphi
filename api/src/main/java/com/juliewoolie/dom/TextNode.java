28a344ae97ce676195f8d24681aac2f9becfebaf
JulieWoolie <andero.magi.420@gmail.com>
2025-12-04 14:11:17 +0200

    Add javadoc to TextNode

--- api/src/main/java/com/juliewoolie/dom/TextNode.java
+++ api/src/main/java/com/juliewoolie/dom/TextNode.java
@@ -2,10 +2,21 @@ package com.juliewoolie.dom;
 
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Represents a basic text node in the XML tree
+ */
 public interface TextNode extends Node {
 
+  /**
+   * Get the text content of the node
+   * @return String text content
+   */
   @Nullable
   String getTextContent();
 
+  /**
+   * Set the text content of the node
+   * @param textContent Text content
+   */
   void setTextContent(@Nullable String textContent);
 }
